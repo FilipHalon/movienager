747606c616dff63d0987a03124cd05cb97d06373
halon <halon.filip@gmail.com>
2020-04-08 13:06:57 +0200

    User model added to the settings.

--- movienager/movienager/settings.py
+++ movienager/movienager/settings.py
@@ -27,7 +27,6 @@ DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
-
 # Application definition
 
 DJANGO_APPS = [
@@ -128,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'accounts.User'

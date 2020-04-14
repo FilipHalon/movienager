184af347cd0e1eed5de2aa9dc2c33e8120fb4c38
halon <halon.filip@gmail.com>
2020-04-14 12:51:38 +0200

    Sidebar revised and adjusted.

--- movienager/accounts/urls.py
+++ movienager/accounts/urls.py
@@ -4,11 +4,11 @@ import accounts.views as views
 
 urlpatterns = [
     path("", views.index, name="main-page"),
-    path("signup", views.SignUpView.as_view()),
+    path("signup", views.SignUpView.as_view(), name="signup"),
     path("signin", views.SignInView.as_view(), name="signin"),
     path("logout", views.UserLogoutView.as_view(), name="logout"),
     path("user_management", views.UserManagementView.as_view(), name="user_management"),
-    path("user_list", views.UserListView.as_view()),
+    path("user_list", views.UserListView.as_view(), name="user-list"),
     re_path(r"^user_edit/(?P<pk>[\w@\.]+$)", views.UserEditView.as_view(), name="user-edit"),
     re_path(r"^user_delete/(?P<pk>[\w@\.]+$)", views.UserDeleteView.as_view(), name="user-delete")
 ]

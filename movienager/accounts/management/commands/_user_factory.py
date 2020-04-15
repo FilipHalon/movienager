2d545275cb609942afbc78d11e0743e0a87c576e
halon <halon.filip@gmail.com>
2020-04-15 12:34:20 +0200

    populatewithusers fixed.

--- movienager/accounts/management/commands/_user_factory.py
+++ movienager/accounts/management/commands/_user_factory.py
@@ -1,28 +1,13 @@
-# import factory  
-
-# from accounts.models import User
-
-# class UserFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = User
-
-#     first_name = factory.Faker('first_name'),
-#     last_name = factory.Faker('last_name'),
-#     email = factory.Faker('email'),
-#     username = factory.Faker('word'),
-#     password = "fakepassword"
-
-from faker import Faker
+import factory  
 
 from accounts.models import User
 
-fake = Faker()
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
 
-def create_user():
-    User.objects.create_user(
-        email = fake.email(),
-        username = fake.word(),
-        password = 'fakepassword',
-        first_name = fake.first_name(),
-        last_name = fake.last_name()
-    )
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    username = factory.Faker('word')
+    password = "fakepassword"
--- movienager/accounts/management/commands/populatewithusers.py
+++ movienager/accounts/management/commands/populatewithusers.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
-# from ._user_factory import UserFactory
-from ._user_factory import create_user
+from ._user_factory import UserFactory
+# from ._user_factory import create_user
 
 class Command(BaseCommand):
     help = 'Populates the database with users.'
@@ -16,4 +16,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for _ in range(options['num']):
-            create_user()
+            # create_user()
+            UserFactory.create()

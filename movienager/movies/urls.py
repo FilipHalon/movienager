c055329fb1c734af52a71d36157648cc62e474f5
halon <halon.filip@gmail.com>
2020-04-16 12:11:51 +0200

    MovieAddView created and linked.

--- movienager/movies/urls.py
+++ movienager/movies/urls.py
@@ -4,4 +4,5 @@ import movies.views as views
 
 urlpatterns = [
     path("movies", views.MoviesView.as_view(), name="movies"),
+    path("movies/add", views.MovieAddView.as_view(), name="movie-add"),
 ]
--- movienager/movies/views.py
+++ movienager/movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
+from django.views.generic import edit
 
 from movies.models import Movie
 
@@ -9,3 +11,10 @@ class MoviesView(generic.ListView):
     model = Movie
     template_name = "movie/movies.html"
     context_object_name = "movies"
+
+
+class MovieAddView(edit.CreateView):
+    model = Movie
+    fields = "__all__"
+    success_url = "/movies"
+    template_name = "movie/forms/movie-add.html"

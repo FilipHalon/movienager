a8f1158430e082e4178cf7247c0c5b2256d61edc
halon <halon.filip@gmail.com>
2020-04-17 11:55:04 +0200

    Removed rendundancies in movies urls.

--- movienager/movies/urls.py
+++ movienager/movies/urls.py
@@ -1,15 +1,23 @@
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 
 import movies.views as views
 
+movie_urls = [
+    path("", views.MoviesView.as_view(), name="movies"),
+    path("add", views.MovieAddView.as_view(), name="movie-add"),
+    re_path(r"^edit/(?P<pk>\d+)$", views.MovieEditView.as_view(), name="movie-edit"),
+    re_path(r"^delete/(?P<pk>\d+)$", views.MovieDeleteView.as_view(), name="movie-delete"),
+]
+
+person_urls = [
+    path("", views.PeopleView.as_view(), name="people"),
+    path("add", views.PersonAddView.as_view(), name="person-add"),
+    re_path(r"^edit/(?P<pk>\d+)$", views.PersonEditView.as_view(), name="person-edit"),
+    re_path(r"^delete/(?P<pk>\d+)$", views.PersonDeleteView.as_view(), name="person-delete"),
+    re_path(r"^(?P<pk>\d+)$", views.PersonView.as_view(), name="person"),
+]
+
 urlpatterns = [
-    path("movies", views.MoviesView.as_view(), name="movies"),
-    path("movies/add", views.MovieAddView.as_view(), name="movie-add"),
-    re_path(r"^movies/edit/(?P<pk>\d+$)", views.MovieEditView.as_view(), name="movie-edit"),
-    re_path(r"^movies/delete/(?P<pk>\d+$)", views.MovieDeleteView.as_view(), name="movie-delete"),
-    path("people", views.PeopleView.as_view(), name="people"),
-    path("people/add", views.PersonAddView.as_view(), name="person-add"),
-    re_path(r"^people/edit/(?P<pk>\d+$)", views.PersonEditView.as_view(), name="person-edit"),
-    re_path(r"^people/delete/(?P<pk>\d+$)", views.PersonDeleteView.as_view(), name="person-delete"),
-    re_path(r"^people/(?P<pk>\d+$)", views.PersonView.as_view(), name="person"),
+    path("movies/", include(movie_urls)),
+    path("people/", include(person_urls)),
 ]

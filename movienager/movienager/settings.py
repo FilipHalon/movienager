d1b36c07c067980b293b193cdfcee6bc6e86ffc3
halon <halon.filip@gmail.com>
2020-04-27 13:34:17 +0200

    Database settings changed.

--- movienager/movienager/settings.py
+++ movienager/movienager/settings.py
@@ -79,21 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "movienager.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ["DB_ENGINE"],
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
-        "HOST": os.environ["DB_HOST"],
-        "PORT": os.environ["DB_PORT"],
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
